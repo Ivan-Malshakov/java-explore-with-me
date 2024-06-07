cbb5e0e36f7338b1536e83cacb1b3040a9029ca9
IVAN_MALSHAKOV <vanodude@yandex.ru>
2024-06-07 07:55:01 +0300

    fix: устанить замечания

--- stats/stats-dto/src/main/java/ru/practicum/ewm/dto/stats/EndpointHit.java
+++ stats/stats-dto/src/main/java/ru/practicum/ewm/dto/stats/EndpointHit.java
@@ -3,6 +3,7 @@ package ru.practicum.ewm.dto.stats;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotBlank;
 
@@ -20,5 +21,6 @@ public class EndpointHit {
     private String ip;
 
     @NotBlank
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private String timestamp;
 }
